b19298b1b75aef2a7b512e62bd9a78ca68982762
TheCB4 <cavelle@thecb4.io>
2018-01-06 23:02:11 -0500

    dependencies

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
       // Dependencies declare other packages that this package depends on.
       // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "git@github.com:thecb4/HyperSpace.git", .branch("master")),
+    .package(url: "https://github.com:thecb4/HyperSpace.git", .branch("master")),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
